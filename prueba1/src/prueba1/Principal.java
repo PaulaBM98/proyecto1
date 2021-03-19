a6370e34e0e2008641e3afe8f3deb2a12ddbf622
Usuario 2 <usuario2@entornos.com>
2021-03-19 17:56:27 +0100

    Version Inicial clase Principal

---
+++ prueba1/src/prueba1/Principal.java
@@ -0,0 +1,11 @@
+package prueba1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo Java-Git");
+		// TODO Auto-generated method stub
+
+	}
+
+}
